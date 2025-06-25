447e6624e195d83aada9d03cfaed576dd95da474
Fatming <osimejp@yahoo.co.jp>
2025-06-25 18:39:38 +0800

    for web GRPC

--- ProtoBufferTool/Prebuilt/bin/to_ts_proto.bat
+++ ProtoBufferTool/Prebuilt/bin/to_ts_proto.bat
@@ -1,2 +1,13 @@
 echo "if no file generated please call npm install -g protoc-gen-js protoc-gen-grpc-web "
 protoc -I=proto_place_here proto_place_here/greeter.proto --js_out=import_style=commonjs:./generated_ts_protobuf --grpc-web_out=import_style=typescript,mode=grpcwebtext:./generated_ts_protobuf
+
+
+
+
+
+#protoc -I=proto_place_here proto_place_here/greeter.proto --js_out=import_style=commonjs:.\generated_ts_protobuf --grpc-web_out=import_style=typescript,mode=grpcwebtext:.\generated_ts_protobuf --plugin=protoc-gen-grpc-web=.\node_modules\protoc-gen-grpc-web\bin\protoc-gen-grpc-web.exe
+
+
+#npm install -g grpc-web 
+#npm install google-protobuf @improbable-eng/grpc-web
+#npm install --save-dev protoc-gen-grpc-web
