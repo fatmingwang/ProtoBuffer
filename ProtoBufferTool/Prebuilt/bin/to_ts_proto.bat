13ea1bc600ca0f10afc0e913bff5ecda1af5ffa1
Fatming <osimejp@yahoo.co.jp>
2025-06-30 16:36:53 +0800

    minor change

--- ProtoBufferTool/Prebuilt/bin/to_ts_proto.bat
+++ ProtoBufferTool/Prebuilt/bin/to_ts_proto.bat
@@ -1,5 +1,6 @@
 echo "if no file generated please call npm install -g protoc-gen-js protoc-gen-grpc-web "
-protoc -I=proto_place_here proto_place_here/greeter.proto --js_out=import_style=commonjs:./generated_ts_protobuf --grpc-web_out=import_style=typescript,mode=grpcwebtext:./generated_ts_protobuf
+protoc -I=proto_place_here proto_place_here/*.proto --js_out=import_style=commonjs:./generated_ts_protobuf --grpc-web_out=import_style=typescript,mode=grpcwebtext:./generated_ts_protobuf
+
 
 
 
